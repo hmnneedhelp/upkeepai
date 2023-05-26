96979c61017cefcc4c081d7b492c645f8b0ed038
Anna Fonar <starlineann@gmail.com>
2023-05-26 10:28:18 +0000

    first part of add ML

---
+++ app/ML/model_training.py
@@ -0,0 +1,32 @@
+from data_preprocessing import create_df_1_2, columns_num_inc_model
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from catboost import CatBoostRegressor
+import pandas as pd
+
+def train_num_inc_model(df_1: pd.DataFrame, df_2: pd.DataFrame):
+    """Train the Model for predict number of incidents for 1 year for every object"""
+
+    target_col, num_cols, cat_cols = columns_num_inc_model()
+
+    df = create_df_1_2(df_1, df_2)
+    df = df[num_cols + cat_cols + target_col].drop_duplicates()
+
+    # Сделаем разбиение оригинальных данных на тренировочную и тестовую выборку.
+    X_train, X_test, y_train, y_test = train_test_split(df[num_cols + cat_cols], df[target_col],
+                                                        train_size=0.8,
+                                                        random_state=42)
+
+    # Обучение модели.
+    boosting_model = CatBoostRegressor(n_estimators=200, learning_rate=0.5)
+
+    boosting_model.fit(X_train, y_train, cat_features=cat_cols)
+
+    boosting_model.save_model('Data/num_inc_model')
+
+    # Предсказания
+    y_test_pred = boosting_model.predict(X_test)
+
+    r2 = r2_score(y_test, y_test_pred.round())
+
+    return r2
---
+++ app/ML/test.py
@@ -0,0 +1,19 @@
+from model_training import train_num_inc_model
+from prediction import predict_num_inc_model
+import pandas as pd
+
+'''download the files'''
+# load data for prediction
+df_1 = pd.read_excel('Data/1_Многоквартирные дома с технико-экономическими характеристиками.xlsx')
+df_2 = pd.read_excel('Data/2_Инциденты,_зарегистрированные_на_объектах_городского_хозяйства.xlsx')
+df_3 = pd.read_excel('Data/3_Работы по капитальному ремонту, проведенные в многоквартирных домах.xlsx')
+df_4 = pd.read_excel('Data/4_Виды работ по капитальному ремонту многоквартирных домов.xlsx', header = 1)
+df_4a = pd.read_excel('Data/4_Виды работ по содержанию и ремонту общего имущества многоквартирных домов.xlsx')
+df_5 = pd.read_excel('Data/5_Типы событий, регистрируемых по типу объекта многоквартирный дом.xlsx')
+
+# train new model
+#r2 = train_num_inc_model(df_1, df_2)
+
+# find the best candidates to capital repare 
+# by prediction num_incidents
+df_cap = predict_num_inc_model(df_1)
