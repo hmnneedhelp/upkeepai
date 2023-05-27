f1530fdc31dfbd9e028bc1198baa667abed60e57
trueprogr <ilovememesdintkillme@gmail.com>
2023-05-28 03:13:05 +0900

    bruhmanegod

--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -3,19 +3,31 @@ import Link from "next/link";
 
 export default function Navbar() {
     return(
-        <main className='absolute z-10'>
-            <div className="absolute inset-y-0 left-0 w-16 h-screen bg-slate-100 shadow-inner">
-                <div className="flex col justify-center align-top mt-6">
+        <main className="absolute border-r-2 shadow-lg">
+            <div className="ml-0 w-72 sticky h-screen">
+                <div className="flex justify-between px-5 mt-5">
                     <Link href="/profile">
-                        <Image
-                        className=""
-                        src="/user.png"
-                        alt="Profile icon"
-                        width={32}
-                        height={32}
-                        priority
-                        />
+                        <Image className="inline-block" src="/user.png" alt="Profile icon" width={64} height={64} priority/>
                     </Link>
+                    <span className="self-center font-bold text-lg text-right">Иванов Иван Иванович</span>
+                </div>
+                <div className="w-64 h-16 my-5 border-2 rounded-[15px] text-center border-black text-black mx-auto">
+                    <p className="leading-tight w-40 m-auto py-4 align-middle">Обращения граждан</p>
+                </div>
+                <div className="w-64 h-16 my-5 border-2 rounded-[15px] text-center border-black text-black mx-auto">
+                    <p className="leading-tight w-52 m-auto py-2 align-middle">Работы по содержанию (Техническое обслуживание)</p>
+                </div>
+                <div className="w-64 h-16 my-5 border-2 rounded-[15px] text-center border-black text-black mx-auto">
+                    <p className="leading-tight w-40 m-auto py-4 align-middle">Текущие ремонты</p>
+                </div>
+                <div className="w-64 h-16 my-5 border-2 rounded-[15px] text-center border-black text-black mx-auto">
+                    <p className="leading-tight w-40 m-auto py-4 align-middle">Капитальные ремноты</p>
+                </div>
+                <div className="w-64 h-16 my-5 border-2 rounded-[15px] text-center border-black text-black mx-auto">
+                    <p className="leading-tight w-40 m-auto py-4 align-middle">Объекты</p>
+                </div>
+                <div className="w-64 h-16 my-5 border-2 rounded-[15px] text-center border-black text-black mx-auto">
+                    <p className="leading-tight w-40 m-auto py-4 align-middle">Аналитика</p>
                 </div>
             </div>
         </main>
--- app/frontend/src/pages/addresses/index.tsx
+++ app/frontend/src/pages/addresses/index.tsx
@@ -1,30 +1,61 @@
-import getUserData from './[id]';
-import Head from 'next/head' 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
-export async function getServerSideProps() {
-    const response = await fetch("http://46.243.227.95:8000/objects/");
-    const data = await response.json();
 
-    return{
-        props:{
-            addresses:data
-        }
+const Addresses = ({ initialData }) => {
+    const [data, setData] = useState(initialData);
+    const [limit, setLimit] = useState(15);
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`http://46.243.227.95:8000/objects/?limit=${limit}&offset=${offset}`);
+            const newData = await response.json();
+            setData(newData);
+        };
+
+        fetchData();
+    }, [limit, offset]);
+
+    const handleClickPlus = () => {
+        setOffset(offset + 15);
+    };
+    const handleClickMinus = () =>{
+        setOffset(offset - 15)
     }
-}
 
-export default function Addresses( {addresses}){
-    return(
-        <div className='bg-white'>
-            {addresses?.leng == 0 ? (
-                <div>Loading</div>
-            ):(
-                addresses?.map(addresses =>(
-                    <div key={addresses.id}>
-                      <Link href={'/addresses/'+ addresses.id}>  <p>{addresses.id} : {addresses.name}</p></Link>
+    return (
+        <div className='bg-white flex flex-col justify-center items-center mt-10'>
+            {data?.length === 0 ? (
+                <div>Загружается</div>
+            ) : (
+                data?.map(address => (
+                    <div  key={address.id}>
+                        <Link href={'/addresses/'+ address.id}>
+                            <p className='text-left'>{address.name}</p>
+                        </Link>
                     </div>
                 ))
             )}
+            <div className='flex justify-between mt-5'>
+                {offset === 0 ?(
+                    <div></div>
+                ) :(
+                    <button onClick={handleClickMinus} className="mr-10 text-blue-500">Предыдущие</button>
+                )}
+                 {offset === 195 ?(
+                    <div></div>
+                ) :(
+                <button onClick={handleClickPlus} className="text-blue-500">Следующие</button>
+                )}
+            </div>
         </div>
     )
-}
+};
+
+Addresses.getInitialProps = async () => {
+    const response = await fetch("http://46.243.227.95:8000/objects/?limit=15&offset=0");
+    const initialData = await response.json();
+    return { initialData };
+};
+
+export default Addresses;
